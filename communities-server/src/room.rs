0b7f53308adcf10133fb5279d5c9d802296543ba
Sudarsan <sudar.theone@gmail.com>
2020-04-10 10:43:54 +0800

    adding a tcp client

--- communities-server/src/room.rs
+++ communities-server/src/room.rs
@@ -1,7 +1,7 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::io::Result;
-use std::rc::Rc;
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
 
 pub struct Room<CS: ClientStore> {
     clients: CS,
@@ -13,27 +13,46 @@ pub struct Message<'a> {
 }
 
 pub trait Client {
-    fn send(&self, message: Message) -> Result<()>;
-    fn receive(&self) -> Result<Message>;
+    fn send(&mut self, message: Message) -> Result<()>;
+    fn receive(&mut self) -> Result<Message>;
 }
 
 pub trait ClientStore {
     fn new() -> Self;
-    fn broadcast(&self, message: Message) -> Result<()>;
+    fn broadcast(&self, cl: impl Client, message: Message) -> Result<()>;
     fn add(&mut self, id: &str) -> Result<()>;
     fn remove(&mut self, id: &str) -> Result<()>;
 }
 
-pub struct SimpleClient {}
+pub struct TCPClient {
+    stream: TcpStream,
+}
 
-impl Client for SimpleClient {
-    fn send(&self, message: Message) -> Result<()> {
+impl Client for TCPClient {
+    fn send(&mut self, message: Message) -> Result<()> {
         println!("send called");
         Ok(())
     }
 
-    fn receive(&self) -> Result<Message> {
-        println!("receive called");
+    fn receive(&mut self) -> Result<Message> {
+        let mut data = [0; 50];
+        while match self.stream.read(&mut data) {
+            Ok(size) => {
+                if size > 0 {
+                    println!("echoed: {:?}\n", &data[0..size]);
+                }
+                true
+            }
+            Err(_) => {
+                println!(
+                    "An error occurred, terminating connection with {}",
+                    self.stream.peer_addr().unwrap()
+                );
+                self.stream.shutdown(Shutdown::Both).unwrap();
+                false
+            }
+        } {}
+
         Ok((Message {
             id: "abcd",
             body: "test",
@@ -41,21 +60,18 @@ impl Client for SimpleClient {
     }
 }
 
-pub struct MemoryStore<Cl: Client> {
-    clients: Rc<RefCell<HashMap<String, Cl>>>,
+pub struct MemoryStore {
+    clients: HashMap<String, String>,
 }
 
-impl<Cl> ClientStore for MemoryStore<Cl>
-where
-    Cl: Client,
-{
+impl ClientStore for MemoryStore {
     fn new() -> Self {
         MemoryStore {
-            clients: Rc::new(RefCell::new(HashMap::new())),
+            clients: HashMap::new(),
         }
     }
 
-    fn broadcast(&self, message: Message) -> Result<()> {
+    fn broadcast(&self, cl: impl Client, message: Message) -> Result<()> {
         Ok(())
     }
 
@@ -64,6 +80,7 @@ where
     }
 
     fn remove(&mut self, id: &str) -> Result<()> {
+        self.clients.remove(id);
         Ok(())
     }
 }
