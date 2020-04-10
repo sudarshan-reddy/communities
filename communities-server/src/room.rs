8246c682011b0035173a1387fc886c7e7773a0b5
Sudarsan <sudar.theone@gmail.com>
2020-04-10 08:21:26 +0800

    added implementation of memorystore

--- communities-server/src/room.rs
+++ communities-server/src/room.rs
@@ -1,4 +1,7 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
 use std::io::Result;
+use std::rc::Rc;
 
 pub struct Room<CS: ClientStore> {
     clients: CS,
@@ -15,8 +18,52 @@ pub trait Client {
 }
 
 pub trait ClientStore {
-    fn new(client: dyn Client) -> Self;
+    fn new() -> Self;
     fn broadcast(&self, message: Message) -> Result<()>;
     fn add(&mut self, id: &str) -> Result<()>;
     fn remove(&mut self, id: &str) -> Result<()>;
+}
+
+pub struct SimpleClient {}
+
+impl Client for SimpleClient {
+    fn send(&self, message: Message) -> Result<()> {
+        println!("send called");
+        Ok(())
+    }
+
+    fn receive(&self) -> Result<Message> {
+        println!("receive called");
+        Ok((Message {
+            id: "abcd",
+            body: "test",
+        }))
+    }
+}
+
+pub struct MemoryStore<Cl: Client> {
+    clients: Rc<RefCell<HashMap<String, Cl>>>,
+}
+
+impl<Cl> ClientStore for MemoryStore<Cl>
+where
+    Cl: Client,
+{
+    fn new() -> Self {
+        MemoryStore {
+            clients: Rc::new(RefCell::new(HashMap::new())),
+        }
+    }
+
+    fn broadcast(&self, message: Message) -> Result<()> {
+        Ok(())
+    }
+
+    fn add(&mut self, id: &str) -> Result<()> {
+        Ok(())
+    }
+
+    fn remove(&mut self, id: &str) -> Result<()> {
+        Ok(())
+    }
 }
