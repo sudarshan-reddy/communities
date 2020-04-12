501587f53f6a04d2a6ff4c2940108e84d5abea66
Sudarsan <sudar.theone@gmail.com>
2020-04-12 16:24:58 +0800

    fixed a few minor bugs

--- communities-server/src/main.rs
+++ communities-server/src/main.rs
@@ -1,9 +1,9 @@
-use std::io::{Read, Write};
+use std::io::{ErrorKind, Read, Write};
 use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
 
-const LOCAL: &str = "127.0.0.1:6142";
+const LOCAL: &str = "127.0.0.1:12233";
 const MSG_SIZE: usize = 32;
 
 fn main() {
@@ -13,33 +13,45 @@ fn main() {
     let mut clients = vec![];
     let (tx, rx) = mpsc::channel::<String>();
 
-    for (mut socket, addr) in listener.accept() {
-        println!("client: {} connected...", addr);
+    loop {
+        if let Ok((mut socket, addr)) = listener.accept() {
+            println!("client: {} connected...", addr);
 
-        let tx = tx.clone();
-        clients.push(socket.try_clone().expect("failed to clone client"));
+            let tx = tx.clone();
+            clients.push(socket.try_clone().expect("failed to clone client"));
 
-        thread::spawn(move || loop {
-            let mut buf = vec![0; MSG_SIZE];
-            match socket.read_exact(&mut buf) {
-                Ok(_) => {}
-                Err(e) => {
-                    println!("closing connection with: {} , err: {}", addr, e);
-                    break;
+            thread::spawn(move || loop {
+                let mut buf = vec![0; MSG_SIZE];
+                match socket.read(&mut buf) {
+                    Ok(_) => {
+                        let msg = buf.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+
+                        println!("{}: {:?}", addr, msg);
+                        tx.send(msg).expect("failed to send msg to rx");
+                    }
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(e) => {
+                        println!("{} , err: {}", addr, e);
+                        break;
+                    }
                 }
-            }
-        });
-    }
+                thread::sleep(::std::time::Duration::from_millis(100));
+            });
+        }
 
-    for msg in rx.try_recv() {
-        clients = clients
-            .into_iter()
-            .filter_map(|mut client| {
-                let mut buf = msg.clone().into_bytes();
-                buf.resize(MSG_SIZE, 0);
+        if let Ok(msg) = rx.try_recv() {
+            clients = clients
+                .into_iter()
+                .filter_map(|mut client| {
+                    let mut buf = msg.clone().into_bytes();
+                    buf.resize(MSG_SIZE, 0);
 
-                client.write_all(&buf).map(|_| client).ok()
-            })
-            .collect::<Vec<_>>();
+                    client.write_all(&buf).map(|_| client).ok()
+                })
+                .collect::<Vec<_>>();
+        }
+
+        thread::sleep(::std::time::Duration::from_millis(100));
     }
 }
